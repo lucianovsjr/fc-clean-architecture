c25ae3a62a92a29c026f1529267b7eb1e47752af
lucianovsjr <lucianovsjr@gmail.com>
2023-08-20 11:57:48 -0300

    Preparando teste de criação de Customer

---
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -0,0 +1,38 @@
+const input = {
+    name: "John",
+    address: {
+        street: "street",
+        city: "city",
+        number: 123,
+        zip: "255",
+    }
+}
+
+const MockRepository = () => {
+    return {
+        find: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+    }
+}
+
+describe("Unit test customer use case", () => {
+    it("should create a customer", async () => {
+        const customerRepository = MockRepository();
+        const customerCreateUseCase = new CustomerCreateUseCase(customerRepository);
+
+        const output = await customerCreateUseCase.execute(input)
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            address: {
+                street: input.address.street,
+                city: input.address.city,
+                number: input.address.number,
+                zip: input.address.zip,
+            }
+        });
+    });
+});
