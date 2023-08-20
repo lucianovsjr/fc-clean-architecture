9856573695f75f76fc39cd362d84dc711f07e13d
lucianovsjr <lucianovsjr@gmail.com>
2023-08-20 12:16:47 -0300

    Implementando testes de criar customer quando temos dados inválidos

--- src/usecase/customer/create/create.customer.unit.spec.ts
+++ src/usecase/customer/create/create.customer.unit.spec.ts
@@ -37,4 +37,22 @@ describe("Unit test customer use case", () => {
             }
         });
     });
+
+    it("should thown an error when name is required", async () => {
+        const customerRepository = MockRepository();
+        const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+        input.name = "";
+
+        await expect(customerCreateUseCase.execute(input)).rejects.toThrow("Name is required");
+    });
+
+    it("should thown an error when street is required", async () => {
+        const customerRepository = MockRepository();
+        const customerCreateUseCase = new CreateCustomerUseCase(customerRepository);
+
+        input.address.street = "";
+
+        await expect(customerCreateUseCase.execute(input)).rejects.toThrow("Street is required");
+    });
 });
