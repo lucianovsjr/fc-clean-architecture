de684e814813c92c36487a6ba285be68aeae1682
lucianovsjr <lucianovsjr@gmail.com>
2023-09-01 07:18:02 -0300

    Testando listagem de Customers

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -40,4 +40,50 @@ describe("E2E test for customer", () => {
 
         expect(response.status).toBe(500);
     });
+
+    it("should lista all customer", async () => {
+        const response = await request(app)
+            .post("/customer")
+            .send({
+                name: "Jhon",
+                address: {
+                    street: "Street",
+                    city: "City",
+                    number: 123,
+                    zip: "12345",
+                }
+            });
+        expect(response.status).toBe(200);
+
+        const response2 = await request(app)
+            .post("/customer")
+            .send({
+                name: "Jane",
+                address: {
+                    street: "Street 2",
+                    city: "City 2",
+                    number: 1234,
+                    zip: "123456",
+                }
+            });
+        expect(response2.status).toBe(200);
+
+        const listResponse = await request(app).get("/customer").send();
+        expect(listResponse.status).toBe(200);
+        expect(listResponse.body.customers.length).toBe(2);
+
+        const customer = listResponse.body.customer[0];
+        expect(customer.name).toBe("Jhon");
+        expect(customer.address.street).toBe("Street");
+        expect(customer.address.city).toBe("City");
+        expect(customer.address.number).toBe(123);
+        expect(customer.address.zip).toBe("12345");
+
+        const customer2 = listResponse.body.customer[1];
+        expect(customer2.name).toBe("Jhon");
+        expect(customer2.address.street).toBe("Street");
+        expect(customer2.address.city).toBe("City");
+        expect(customer2.address.number).toBe(123);
+        expect(customer2.address.zip).toBe("12345");
+    })
 });
