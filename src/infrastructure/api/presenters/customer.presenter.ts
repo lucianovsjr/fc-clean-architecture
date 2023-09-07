e7a6c99b182c865f63b18cf5d0219ab0d25114b8
lucianovsjr <lucianovsjr@gmail.com>
2023-09-07 12:42:25 -0300

    Criando Presenter para listagem de customer

---
+++ src/infrastructure/api/presenters/customer.presenter.ts
@@ -0,0 +1,33 @@
+import { toXML } from "jstoxml";
+import { OutputListCustomerDto } from "../../../usecase/customer/list/list.customer.dto";
+
+
+export default class CustomerPresenter {
+    static toXML(data: OutputListCustomerDto): string {
+        const xmlOptions = {
+            header: true,
+            indent: " ",
+            newLine: "\n",
+            allowEmpty: true,
+        };
+
+        const xmlElements = {
+            customers: {
+                customers: {
+                    customer: data.customers.map(customer => ({
+                        id: customer.id,
+                        name: customer.name,
+                        address: {
+                            street: customer.address.street,
+                            number: customer.address.number,
+                            zip: customer.address.zip,
+                            city: customer.address.city,
+                        },
+                    })),
+                },
+            },
+        };
+
+        return toXML(xmlElements, xmlOptions);
+    }
+}
